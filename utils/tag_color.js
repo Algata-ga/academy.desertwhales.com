c89fa6630637bb1cc6d51c895f45dc1bcfd30c99
Alfred Jophy <alfredjophy@protonmail.com>
2022-06-11 15:43:31 +0530

    fixed tag colors

--- utils/tag_color.js
+++ utils/tag_color.js
@@ -5,15 +5,20 @@ const color_set = [
     "#63C083",
     "#C06363",
     "#C0B763",
+    "#C06363",
+    "#63C083",
+    "#6388C0",
+    "#939278",
+    "#9D63C0",
 ];
 
 function hashCode(str) {
     let hash = 0;
     for (let i = 0, len = str.length; i < len; i++) {
         let chr = str.charCodeAt(i);
-        hash = (hash << 5) - hash + chr;
-        hash |= 0; // Convert to 32bit integer
+        hash += 17 * chr + (chr % 3) + (chr - 3) * chr * (chr + 3);
     }
+    hash = hash - str.length;
     return hash;
 }
 
