1fa43dd4aea02adc78d9e78857e7d3e33a7ac4fb
Alfred Jophy <alfredjophy@protonmail.com>
2022-06-15 21:41:10 +0530

    Removed List/Card switcher for mobiles

--- components/SearchResultsView.jsx
+++ components/SearchResultsView.jsx
@@ -7,15 +7,14 @@ const SearchResultsView = ({ data }) => {
 
     return (
         <div>
-            <div className={style.viewbox}>
-                <h4>view | </h4>
-            <button onClick={() => setListView(() => !isListView)}>
-                {isListView ? "Card" : "List"}
-            </button>
-            <button onClick={() => setListView(() => !isListView)}>
-                {isListView ? "Card" : "List"}
-            </button>
-            </div>
+            {window.matchMedia("(min-width:768px)").matches && (
+                <div className={style.viewbox}>
+                    <h4>view | </h4>
+                    <button onClick={() => setListView(() => !isListView)}>
+                        {isListView ? "Card" : "List"}
+                    </button>
+                </div>
+            )}
             <div className={isListView ? style.searchboxlist : style.searchbox}>
                 {data.length == 0 ? (
                     <h2>Not articles found</h2>
